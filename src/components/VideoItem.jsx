e6fa0d9ffd67dfce93077b1a5ad421c490c99827
Kelvin-Ben <kevoben14@gmail.com>
2023-04-24 17:25:23 +0300

    Used prettier to clean the code base

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './VideoItem.css';
-const VideoItem = ({video, onVideoSelect}) => {
+import React from "react";
+import "./VideoItem.css";
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div onClick={() =>onVideoSelect(video)} className='video-item item'>
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
-      <div className='content'>
-        <div className='header'>{video.snippet.title}</div>
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default VideoItem;
